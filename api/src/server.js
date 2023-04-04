0d62c96c92992177b0936865cd79c699e380d140
Luis <luismanlaz@hotmail..com>
2023-04-04 18:15:38 -0300

    1° commit

--- api/src/server.js
+++ api/src/server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header('Access-Control-Allow-Headers', '*');
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
