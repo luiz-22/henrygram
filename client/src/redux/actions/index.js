5632cdf10ed6e5e23efc1289a03b2b22a26997a3
Luis <luismanlaz@hotmail..com>
2023-04-04 17:30:50 -0300

    1° commit

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -48,8 +48,7 @@ export const SET_LOADING = "SET_LOADING";
 //USERS INFORMATION
 //REGISTER
 
- const URL = import.meta.env.VITE_URL_RAILWAY;
-
+const URL = import.meta.env.VITE_URL_RAILWAY;
 
 export const createUser = (user) => {
   return async function (dispatch) {
@@ -497,8 +496,7 @@ export const likeDislikePostAction = (data) => {
   return async (dispatch) => {
     try {
       await axios.post(
-        `${URL || "http://localhost:3000"}/api/posts/like?postId=${
-          data.postId
+        `${URL || "http://localhost:3000"}/api/posts/like?postId=${data.postId
         }&userId=${data.userId}`,
         data
       );
